eb3b05b5a27f31b939579ac56ca9d779166da429
Joren <rothmanjoren@gmail.com>
2024-09-09 09:38:08 +0200

    Update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,10 +3,10 @@ local config = {}
 
 config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font 'Fira Code'
+config.font_size = 16
 config.enable_tab_bar = false
 
 config.window_frame = {
-
     font = wezterm.font 'Fira Code',
     font_size = 12.0,
 }
