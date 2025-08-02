379a59f26adf057b56cd572142857b0dad1f333a
Joren <rothmanjoren@gmail.com>
2025-08-02 15:27:51 +0200

    feat: disable zelij startup tips

--- config/zsh/config.d/asdf.zsh
+++ config/zsh/config.d/asdf.zsh
@@ -1,4 +1,10 @@
-. ~/.asdf/plugins/java/set-java-home.zsh
+# Check if the Java plugin set-java-home file exists before sourcing
+if [[ -f "${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/java/set-java-home.zsh" ]]; then
+  source "${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/java/set-java-home.zsh"
+fi
 
+# Add asdf shims to PATH
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+
+# Add asdf completions to fpath
 fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
